975d03d250b8aa89f0c88ee4499067d5e6438cd3
Tomasz <estewez@gmail.com>
2018-02-23 19:10:41 +0100

    Fixed CSS issue when form was leaking out of the screen if you added more items than could fit in 100vh. Key attribute of list is now on proper item (ToDo istead of li)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Todo.css';
 
 const Todo = (props) => {
-    return <li className={style.Todo} key={props.id} onClick={() => props.remove(props.id)}>{props.text}</li>;
+    return <li className={style.Todo} onClick={() => props.remove(props.id)}>{props.text}</li>;
 }
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,16 +5,13 @@ import Todo from './Todo';
 const TodoList = props => {
     return (
         <ul className={style.TodoList}>
-            <Todo
-                
-            />
             {props.data.map(listValue => {
                 return <Todo
+                    key = {listValue.id}
                     text = {listValue.text}
                     remove = {props.remove}
                     id = {listValue.id}
                 />
-                //<li className={style.TodoItem} key={listValue.id} onClick={() => props.remove(listValue.id)}>{listValue.text}</li>;
             })}
         </ul>
     );
